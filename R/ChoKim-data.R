d97f727bd62077a450d894f77a0e07742260c336
eunscho <46369547+eunscho@users.noreply.github.com>
2022-01-18 12:03:45 +0900

    Add files via upload

---
+++ R/ChoKim-data.R
@@ -0,0 +1,10 @@
+#' Cho and Kim's (2015) artificial unidimensional data of five items
+#'
+#' A hypothetical data consisting of five items.
+#' @docType data
+#' @usage data(ChoKim)
+#' @source Cho, E., & Kim, S. (2015). Cronbach’s coefficient alpha: Well known
+#' but poorly understood. Organizational Research Methods, 18(2), 207-230.
+#' @author Eunseong Cho, \email{bene@kw.ac.kr}
+#'
+"ChoKim"
