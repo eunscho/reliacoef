88602413668983caf7124b4df4d2cef330906574
eunscho <46369547+eunscho@users.noreply.github.com>
2022-01-18 12:04:21 +0900

    Add files via upload

---
+++ R/HancockMueller-data.R
@@ -0,0 +1,14 @@
+#' Hancock and Mueller's (2001)  unidimensional data of four items
+#'
+#' Hancock and Mueller's artificial unidimensional dataset consisting of four 
+#' items. 
+#' 
+#' @docType data
+#' @usage data(HancockMueller)
+#' @source Hancock, G., & Mueller, R. O. (2001). Rethinking construct 
+#' reliability within latent variable systems. In R. Cudeck, S. du Toit, & D. 
+#' Sörbom (Eds.), Structural equation modeling: Present and future—A festschrift 
+#' in honor of Karl Jöreskog (pp. 195–216). Scientific Software International. 
+#' @author Eunseong Cho, \email{bene@kw.ac.kr}
+#'
+"HancockMueller"
---
+++ R/Kristof-data.R
@@ -0,0 +1,14 @@
+#' Kristof's (1971) unidimensional data
+#'
+#' Kristof's (1971) unidimensional data consisting of three items.This dataset
+#' is the result of the Scholastic Aptitude Test and is taken from Jöreskog (1978).
+#'
+#' @docType data
+#' @usage data(Kristof)
+#' @references Kristof, W. On the theory of a set of tests which differ only in
+#' length. Psychometrika, 1971, 36, 207-225.
+#' @references Jöreskog, K. G. (1978). Structural analysis of covariance and
+#' correlation matrices. Psychometrika, 43(4), 443-477.
+#' @author Eunseong Cho, \email{bene@kw.ac.kr}
+#'
+"Kristof"
